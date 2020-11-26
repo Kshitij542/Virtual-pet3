76c093b930c2c493ff8249c451c2189ae7baf156
Kshitij542 <71619725+Kshitij542@users.noreply.github.com>
2020-11-26 22:25:05 +0530

    Update food.js

--- food.js
+++ food.js
@@ -1,41 +1,58 @@
-class Food {
+class Foods{
     constructor(){
-    this.foodStock=0;
-    this.lastFed;
-    this.image=loadImage('images/Milk.png');
+        this.foodStock=0;
+        this.lastFed;
+        this.image=loadImage("Milky.png")
     }
-  
-   updateFoodStock(foodStock){
-    this.foodStock=foodStock;
-   }
-  
-   getFedTime(lastFed){
-     this.lastFed=lastFed;
-   }
-  
-   deductFood(){
-     if(this.foodStock>0){
-      this.foodStock=this.foodStock-1;
-     }
-    }
-  
     getFoodStock(){
-      return this.foodStock;
+        return this.foodStock
     }
-  
-    display(){
-      var x=80,y=100;
-      
-      imageMode(CENTER);   
-      if(this.foodStock!=0){
-        for(var i=0;i<this.foodStock;i++){
-          if(i%10==0){
-            x=350;
-            y=y+50;
-          }
-          image(this.image,x,y,50,50);
-          x=x+30;
+
+    getFedTime(lastFed){
+        this.lastFed=lastFed;
+    }
+
+    updateFoodStock(foodStock){
+      this.foodStock=foodStock;
+    }
+
+    deductFood(){
+        if(this.foodStock>0){
+            this.foodStock=this.foodStock-1;
         }
-      }
+
     }
-  }
+
+    bedroom(){
+        background(bedroom,550,500)
+    }
+
+    garden(){
+        background(garden,550,500)
+    }
+
+    washroom(){
+        background(washroom,550,500)
+    }
+
+    livingRoom(){
+        background(livingRoom,550,500)
+    }
+
+    display(){
+        var x=200,y=100
+        imageMode(CENTER)
+        image(this.image,720,220,70,70);
+
+        if (this.foodStock!==0) {
+            for (var i=0;i<this.foodStock;i++){
+                if(i%10===0){
+                    x=20;
+                    y=y+50;
+                }
+                image(this.image,x,y,50,50)
+                x=x+30
+            }
+        }
+    }
+}
